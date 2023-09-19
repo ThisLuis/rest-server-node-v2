b165b5920ce5ffd581f1a617cad058e88787223d
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-09-18 20:49:41 -0600

    refactor(backend): rename _id to uid

--- models/user.js
+++ models/user.js
@@ -39,4 +39,10 @@ const UserSchema = Schema({
 
 });
 
+UserSchema.methods.toJSON = function() {
+	const { __v, password, _id, ...user } = this.toObject();
+	user.uid = _id;
+	return user;
+}
+
 module.exports = model('User', UserSchema);
